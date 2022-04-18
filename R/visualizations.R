4daf12de88e5a469d0749ebaf7d12e9e4f660b05
Jack Carter <jack-p-carter@outlook.com>
2022-04-18 22:52:00 +0300

    updating files

--- R/visualizations.R
+++ R/visualizations.R
@@ -3,175 +3,127 @@ library(tidyverse)
 library(ggplot2)
 library(stringr)
 library(ggthemes)
+library(knitr)
 library(here)
 
-# gets the z score for a specific term. 
+# gets the z score for each term. 
 get_z_score <- function(data) {
-  
-  mean <- mean(
-    data
-    )
-  sigma <- sd(
-    data
-  )
+  mean <- mean(data)
+  sigma <- sd(data)
   z_scores <- list()
   for(i in 1:length(data)) {
     z_scores[i] <- (data[i] - mean) / sigma
   }
-  return(unlist(
-    z_scores
-    ))
+  return(unlist(z_scores))
 }
 
 # gets the z score for a list of terms. 
 get_z_scores <- function(data) {
-  
   groups <- data %>%
     group_split(term)
-  
   z_scores <- list()
   for(i in 1:length(groups)) {
-    z_scores[i] <- list(
-      get_z_score(groups[[i]]$hits)
-      )
+    z_scores[[i]] <- list(get_z_score(groups[[i]]$hits))
   }
-  return(unlist(
-    z_scores
-    ))
+  return(unlist(z_scores))
 }
 
-# good terms. 
-the_good <- c("patriotism",
-              "national identity",
-              "nationalism")
-
-# bad terms. 
-the_bad <- c("conspiracy",
-             "false information",
-             "misinformation",
-             "disinformation")
-
-# ugly terms. 
-the_ugly <- c("white supremacy",
-              "homophobia",
-              "discrimination",
-              "misogyny",
-              "transphobia",
-              "hate speech")
+national_belonging <- c("patriotism",
+                        "national identity")
+partisanship <- c("partisanship",
+                  "political polarization",
+                  "political divide",
+                  "political differences")
+discrimination <- c("anti-semitism",
+                    "sexism", 
+                    "racism", 
+                    "islamophobia",
+                    "transphobia")
 
 # read in data. 
-data <- read_csv("data.csv")
+data <- read_csv(here("data.csv"))
 
 # sort data alphabetically by term. 
 index <- order(data$term)
-sorted_data_frame <- data[index,]
+sorted_df <- data[index,]
 
 # change term to upper case. 
-sorted_data_frame$term <- str_to_title(sorted_data_frame$term)
+sorted_df$term <- str_to_title(sorted_df$term)
+
+# adds a z-score variable. 
+sorted_df$z_score <- get_z_scores(sorted_df)
 
 # my personal plot theme for data visualizations. 
 my_theme <- theme_economist_white(gray_bg = FALSE) +
-  theme(plot.title = element_text(hjust = 0.5, 
-                                  vjust = 12, 
-                                  size = 9, 
+  theme(plot.title = element_text(hjust = 0.5,
+                                  vjust = 15,
+                                  size = 11,
                                   color = "#474747"),
         plot.margin = unit(c(1.5, 1, 1.5, 1), "cm"),
-        axis.text = element_text(size = 9, 
+        axis.text = element_text(size = 9,
                                  color = "gray30"),
         axis.text.x=element_text(vjust = -2.5),
-        axis.title.x = element_text(size = 9, 
-                                    color = "gray30", 
+        axis.title.x = element_text(size = 9,
+                                    color = "gray30",
                                     vjust = -10),
-        axis.title.y = element_text(size = 9, 
-                                    color = "gray30", 
+        axis.title.y = element_text(size = 9,
+                                    color = "gray30",
                                     vjust = 10),
         legend.direction = "vertical", 
         legend.position = "right",
         legend.title = element_blank(),
-        legend.text = element_text(size = 12, 
+        legend.text = element_text(size = 11,
                                    color = "gray20"),
         legend.margin=margin(1, -15, 1, 0),
         legend.spacing.x = unit(0.25, "cm"),
         legend.key.size = unit(1, "cm"), 
         legend.key.height = unit(0.75, "cm"),
-        strip.text = element_text(hjust = 0.5, 
-                                  vjust = 1, 
-                                  size = 10, 
+        strip.text = element_text(hjust = 0.5,
+                                  vjust = 1,
+                                  size = 10,
                                   color = "#474747"),
         panel.spacing = unit(2, "lines"))
 
-# the good. 
-sorted_data_frame %>% 
-  mutate(
-    z_score = get_z_scores(sorted_data_frame)
-    ) %>%
-  filter(
-    term %in% str_to_title(the_good)
-    ) %>%
-  ggplot(
-    aes(x=year, y=z_score, col=term)
-    ) +
-  geom_smooth(
-    se=F, span = 0.5, size = 0.5
-    ) +
-  ggtitle(
-    "Identity"
-    ) +
-  ylab(
-    "Z Score"
-    ) +
-  xlab(
-    "Year"
-    ) + 
-  my_theme
-
-# the bad. 
-sorted_data_frame %>% 
-  mutate(
-    z_score = get_z_scores(sorted_data_frame)
-  ) %>%
-  filter(
-    term %in% str_to_title(the_bad)
-  ) %>%
-  ggplot(
-    aes(x=year, y=z_score, col=term)
-  ) +
-  geom_smooth(
-    se=F, span = 0.5, size = 0.5
-  ) +
-  ggtitle(
-    "Distrust"
-  ) +
-  ylab(
-    "Z Score"
-  ) +
-  xlab(
-    "Year"
-  ) + 
-  my_theme
-
-# the ugly. 
-sorted_data_frame %>% 
-  mutate(
-    z_score = get_z_scores(sorted_data_frame)
-  ) %>%
-  filter(
-    term %in% str_to_title(the_ugly)
-  ) %>%
-  ggplot(
-    aes(x=year, y=z_score, col=term)
-  ) +
-  geom_smooth(
-    se=F, span = 0.5, size = 0.5
-  ) +
-  ggtitle(
-    "Divisiveness"
-  ) +
-  ylab(
-    "Z Score"
-  ) +
-  xlab(
-    "Year"
-    ) + 
+# creates a plot with smoothed loess regression lines. 
+make_plot <- function(category, title) {
+  plot <- sorted_df %>%
+    filter(term %in% str_to_title(category)) %>%
+    ggplot(aes(x=year, 
+               y=z_score, 
+               col=term)) +
+    geom_smooth(se=F, 
+                span = 0.5, 
+                size = 0.5) +
+    geom_vline(xintercept = 2016,
+               size=0.25,
+               col="#696969") +
+    geom_text(aes(x=2016, 
+                  label="Trump's 2016 win -", 
+                  y=max(z_score)+0.1,
+                  hjust=1.05),
+              size=3,
+              col="#696969") +
+    geom_vline(xintercept = 2020,
+               size=0.25,
+               col="#696969") +
+    geom_text(aes(x=2020, 
+                  label="Trump's 2020 loss -",
+                  y=max(z_score)+0.1,
+                  hjust=1.05),
+              size=3,
+              col="#696969") +
+    ggtitle(title) +
+    ylab("Articles (z-scores)") +
+    xlab("") + 
     my_theme
- 
+  return(plot)
+}
+
+# national_belonging  
+make_plot(national_belonging, "National Belonging")
+
+# partisanship   
+make_plot(partisanship, "Partisanship")
+
+# discrimination 
+make_plot(discrimination, "Discrimination")
--- The-Good-the-Bad-and-the-Ugly.md
+++ The-Good-the-Bad-and-the-Ugly.md
@@ -1,44 +1,15 @@
 The Good, the Bad and the Ugly
 ================
 Jack Carter
-10/17/2021
+18/04/2022
 
 ## **Summary**
 
 This project shows how the conversation in the New York Times (NYT)
-turned ugly following the 2016 election of Donald Trump. A strong sense
-of national belonging is undoubtedly good for any country, yet Trump’s
-rise was also accompanied by discrimination, the emboldening of extreme
-groups, political polarization, disinformation and widespread skepticism
-regarding the legitimacy of the 2020 election, the last three of which
-appear here to stay.
-
- 
-
-## **Method**
-
-**1) Data Collection:**
-
-The data used in this study were collected from the New York Times (NYT)
-API, including the number of articles containing certain terms for each
-year between 2011 and 2021.
-
-<br/>
-
-**2) Z Scores:**
-
-The z scores were calculated as the raw number of articles containing a
-certain, less that term’s mean, divided by its standard deviation. This
-standardizes the scores, allowing for a comparison of the relative
-trajectory of terms.
-
-<br/>
-
-**2) Loess Transformation:**
-
-The data for each term is then plotted with the use of a loess
-regression line. This smoothens out the data into a curve for a better
-visualization of overall trends.
+changed following the 2016 election of Donald Trump. A strong sense of
+national belonging is undoubtedly good for any country, yet Trump’s rise
+also appears to have been accompanied by growing political partisanship
+and increased social discrimination.
 
  
 
@@ -48,11 +19,105 @@ visualization of overall trends.
 
 ## 2\) The Bad
 
-![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-2-1.png)<!-- -->![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-2-2.png)<!-- -->
+![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-2-1.png)<!-- -->
 
 ## 3\) The Ugly
 
-![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-3-2.png)<!-- -->![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-3-3.png)<!-- -->
+![](The-Good-the-Bad-and-the-Ugly_files/figure-gfm/unnamed-chunk-3-1.png)<!-- -->
+
+ 
+
+## **Method**
+
+### **1) Data Collection:**
+
+The data were collected from the New York Times API. They include the
+number of articles for each search term in the 12 years between 2011 and
+2022.
+
+**Total articles (thousands)**
+
+| Anti-Semitism | Islamophobia | National Identity | Partisan Politics | Partisanship | Patriotism | Political Differences | Political Divide | Political Polarization | Racism | Sexism | Transphobia |
+| ------------: | -----------: | ----------------: | ----------------: | -----------: | ---------: | --------------------: | ---------------: | ---------------------: | -----: | -----: | ----------: |
+|          2.72 |         0.59 |              1.28 |               0.8 |         2.34 |       2.65 |                  0.44 |              0.5 |                   0.56 |  14.08 |   3.15 |        0.15 |
+
+ 
+
+### **2) Z-scores:**
+
+The number of articles is converted to each term’s z-score. This allows
+us to view the term’s relative distribution over time. It is calculated
+as 1) the number of articles less the term’s mean, 2) divided by the
+term’s standard deviation.
+
+``` r
+# gets the z-score for each term. 
+get_z_score <- function(data) {
+  mean <- mean(data)
+  sigma <- sd(data)
+  z_scores <- list()
+  for(i in 1:length(data)) {
+    z_scores[i] <- (data[i] - mean) / sigma
+  }
+  return(unlist(z_scores))
+}
+
+# gets the z-score for a list of terms. 
+get_z_scores <- function(data) {
+  groups <- data %>%
+    group_split(term)
+  z_scores <- list()
+  for(i in 1:length(groups)) {
+    z_scores[[i]] <- list(get_z_score(groups[[i]]$hits))
+  }
+  return(unlist(z_scores))
+}
+```
+
+ 
+
+### **3) Loess Transformation:**
+
+The data for each term is plotted with the use of a loess regression
+line (geom\_smooth in the code below). This transforms the data into a
+smooth curve for a better visualization of underlying trends.
+
+``` r
+# creates a plot with smoothed loess regression lines. 
+make_plot <- function(category, title) {
+  plot <- sorted_df %>%
+    filter(term %in% str_to_title(category)) %>%
+    ggplot(aes(x=year, 
+               y=z_score, 
+               col=term)) +
+    geom_smooth(se=F, 
+                span = 0.5, 
+                size = 0.5) +
+    geom_vline(xintercept = 2016,
+               size=0.25,
+               col="#696969") +
+    geom_text(aes(x=2016, 
+                  label="Trump's 2016 win -", 
+                  y=max(z_score)+0.1,
+                  hjust=1.05),
+              size=3,
+              col="#696969") +
+    geom_vline(xintercept = 2020,
+               size=0.25,
+               col="#696969") +
+    geom_text(aes(x=2020, 
+                  label="Trump's 2020 loss -",
+                  y=max(z_score)+0.1,
+                  hjust=1.05),
+              size=3,
+              col="#696969") +
+    ggtitle(title) +
+    ylab("Articles (z-scores)") +
+    xlab("") + 
+    my_theme
+  return(plot)
+}
+```
 
  
 
